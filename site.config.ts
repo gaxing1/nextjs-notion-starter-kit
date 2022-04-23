b91a04702cc1fa0db0485c41d6c5b0a544479551
gaxing1 <99528702+gaxing1@users.noreply.github.com>
2022-04-23 18:55:46 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'a3da1c24543349f782d682748e9ab09b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Axing小站',
+  domain: 'https://www.notion.so/Axing-a3da1c24543349f782d682748e9ab09b',
+  author: 'X',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
